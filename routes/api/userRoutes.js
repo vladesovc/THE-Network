13fe473de557759d2e568393024a9db9533ae4aa
Corey <coreyvladesov2023@u.northwestern.edu>
2023-11-17 18:41:09 -0600

    added in user routes code

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  removeFriend,
+  addFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(deleteUser);
+
+  router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
+
+module.exports = router;
